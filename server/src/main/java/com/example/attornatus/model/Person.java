a4b98ff67fbd54cb1d86498f6137b7f4ecf4054a
Yohan Silva <yohanps.dev@gmail.com>
2023-02-01 15:41:59 -0300

    fix: import correct package of Id and configure properties

--- server/src/main/java/com/example/attornatus/model/Person.java
+++ server/src/main/java/com/example/attornatus/model/Person.java
@@ -1,10 +1,6 @@
 package com.example.attornatus.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
 
 import java.util.Date;
 
