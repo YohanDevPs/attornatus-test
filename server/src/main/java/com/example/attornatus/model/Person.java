12fb0d6a8f7ab7bffe137a3ba90f3a23a776f198
Yohan Silva <yohanps.dev@gmail.com>
2023-02-01 15:28:44 -0300

    build: create initial structures of Person

---
+++ server/src/main/java/com/example/attornatus/model/Person.java
@@ -0,0 +1,49 @@
+package com.example.attornatus.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Date birthDate;
+
+    public Person(Long id, String name, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
---
+++ server/src/main/java/com/example/attornatus/service/PersonService.java
@@ -0,0 +1,5 @@
+package com.example.attornatus.service;
+
+public interface PersonService {
+
+}
